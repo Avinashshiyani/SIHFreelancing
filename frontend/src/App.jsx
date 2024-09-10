74cd7129d32e5fc5e4b14aa0c0b32e60d49558d2
Mange Jay <jaymange263@gmail.com>
2024-09-10 06:55:43 +0530

    Creating My Branch And Also Adding My Old Code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login/Login";
 import SignUp from "./Login/SignUp";
+import Home from "./Login/Home";
 import Dashboard from "./Dashboard/Dashboard";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Login />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/home" element={<Home />}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
       </Routes>
     </Router>
